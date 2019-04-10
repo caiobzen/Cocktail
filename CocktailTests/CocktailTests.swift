d3d9057987fc6f70269f26f5ea973b1b4d99345d
Carlos Corrêa <caiobzen@gmail.com>
2019-04-09 22:11:41 -0300

    feat: add cocktail file

--- CocktailTests/CocktailTests.swift
+++ CocktailTests/CocktailTests.swift
@@ -1,8 +1,17 @@
 import Quick
 import Nimble
 
+@testable import Cocktail
+
 class CocktailTests: QuickSpec {
     override func spec() {
-        
+        describe(".Cocktail") {
+            context("#init") {
+                it("creates a Cocktail instance") {
+                    let cocktail = Cocktail()
+                    expect(cocktail).toNot(beNil())
+                }
+            }
+        }
     }
 }
